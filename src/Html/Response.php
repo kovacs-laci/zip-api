724de245befdd8aa8112ff2be719bb511500ffca
Kovács László <149382301+kovacs-laci@users.noreply.github.com>
2026-02-13 18:40:15 +0100

    cors hozzáadása

--- src/Html/Response.php
+++ src/Html/Response.php
@@ -26,6 +26,11 @@ class Response
 
     private static function send(array $data, int $code): void {
         http_response_code($code);
+        // CORS headers
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
         header('Content-Type: application/json');
         echo json_encode(['code' => $code] + $data, JSON_THROW_ON_ERROR);
         exit;
